7259104d97e7945c6baa5b171d16eae8537af127
lee0203-jun <conljm97@gmail.com>
2023-09-03 17:29:09 +0900

    배포

---
+++ js/index.js
@@ -0,0 +1,140 @@
+$(function(){
+
+    $('.sic1 .top_img span').on('click',function(){
+
+        // .top_img가 사라진다,video와 닫기버튼이 나타난다.
+
+        $('.sic1 .top_img').fadeOut();
+
+        $('.sic1 .close').css({'display':'block'});
+        
+
+        $('.sic1 .videoWrap video').show();
+
+      
+
+    });// click
+
+    $('.close').on('click',function(){
+
+
+        // $('.videoWrap video').fadeOut();
+        // $('.close').fadeOut();
+
+        $('.sic1 .close').css({'display':'none'});
+        $('.sic1 .videoWrap video').css({'display':'none'});
+
+        $('.sic1 .top_img').show();
+
+    })
+
+
+
+
+});// doc
+
+$(function(){
+
+    $('.sic2 .top_img span').on('click',function(){
+
+        // .top_img가 사라진다,video와 닫기버튼이 나타난다.
+
+        $('.sic2 .top_img').fadeOut();
+
+        $('.sic2 .close').css({'display':'block'});
+        
+
+        $('.sic2 .videoWrap video').show();
+
+      
+
+    });// click
+
+    $('.close').on('click',function(){
+
+
+        // $('.videoWrap video').fadeOut();
+        // $('.close').fadeOut();
+
+        $('.sic2 .close').css({'display':'none'});
+        $('.sic2 .videoWrap video').css({'display':'none'});
+
+        $('.sic2 .top_img').show();
+
+    })
+
+
+
+
+});// doc
+
+
+$(function(){
+
+    $('.sic3 .top_img span').on('click',function(){
+
+        // .top_img가 사라진다,video와 닫기버튼이 나타난다.
+
+        $('.sic3 .top_img').fadeOut();
+
+        $('.sic3 .close').css({'display':'block'});
+        
+
+        $('.sic3 .videoWrap video').show();
+
+      
+
+    });// click
+
+    $('.close').on('click',function(){
+
+
+        // $('.videoWrap video').fadeOut();
+        // $('.close').fadeOut();
+
+        $('.sic3 .close').css({'display':'none'});
+        $('.sic3 .videoWrap video').css({'display':'none'});
+
+        $('.sic3 .top_img').show();
+
+    })
+
+
+
+
+});// doc
+
+$(function(){
+
+    $('.sic4 .top_img span').on('click',function(){
+
+        // .top_img가 사라진다,video와 닫기버튼이 나타난다.
+
+        $('.sic4 .top_img').fadeOut();
+
+        $('.sic4 .close').css({'display':'block'});
+        
+
+        $('.sic4 .videoWrap video').show();
+
+      
+
+    });// click
+
+    $('.close').on('click',function(){
+
+
+        // $('.videoWrap video').fadeOut();
+        // $('.close').fadeOut();
+
+        $('.sic4 .close').css({'display':'none'});
+        $('.sic4 .videoWrap video').css({'display':'none'});
+
+        $('.sic4 .top_img').show();
+
+    })
+
+
+
+
+});// doc
